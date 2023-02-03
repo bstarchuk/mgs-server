c1fecf9dfa55720957926a12bbc79803293cf0b9
Bogdan Starchuk <bstarchuk95@gmail.com>
2023-02-03 23:55:00 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import Gamedig from 'gamedig';
+import axios from 'axios';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/:platform/:ip', async (req, res) => {
+    try {
+        const {platform, ip} = req.params;
+        const [host, port] = ip.split(':')
+
+        if (platform === 'discord') {
+
+        } else if (platform === 'scum') {
+            axios.get(`https://api.hellbz.de/scum/api.php?address=${host}&port=${port}`)
+                .then((response) => {
+                    res.send({game: platform, ip, data: response.data})
+                })
+                .catch((error) => {
+                    console.log(error);
+                    res.send('Server is offline')
+                });
+        } else {
+            Gamedig.query({
+                type: platform,
+                host,
+                port
+            }).then((response) => {
+                res.send({game: platform, ip, data: response})
+            }).catch((error) => {
+                console.log(error);
+                res.send('Server is offline')
+            });
+        }
+    } catch (e) {
+        console.error(e);
+    }
+});
+
+app.listen(PORT, () => console.log(`Listening on port http://localhost:${PORT}`));
